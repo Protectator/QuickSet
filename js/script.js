20096d10106e5255bdbe8834e3085bb7aff0e3c2
Kewin Dousse <kewin.d@websud.ch>
2014-08-13 20:21:16 +0200

    Adding core files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*	QuickSet - Quickly see and edit League of Legends Item Sets
+    Copyright (C) 2014  Kewin Dousse (Protectator)
+
+    This file is part of QuickSet.
+
+    QuickSet is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or any later version.
+
+    QuickSet is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact : kewin.d@websud.ch
+    Project's repository : https://github.com/Protectator/QuickSet
+*/
